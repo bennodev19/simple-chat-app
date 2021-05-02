2b9bea0e21c153689ece8cf84354d17a1311e049
Benno Kohrs <bennokohrs@gmail.com>
2021-05-02 10:26:52 +0200

    added jest coverage

--- frontend/jest.base.config.js
+++ frontend/jest.base.config.js
@@ -7,6 +7,8 @@ module.exports = {
   transform: {
     '^.+\\.js|jsx|ts|tsx?$': 'ts-jest',
   },
+  // https://github.com/bpedersen/jest-mock-console#readme
+  setupFilesAfterEnv: ['jest-mock-console/dist/setupTestFramework.js'],
   /* https://stackoverflow.com/questions/63904196/esmoduleinterop-flag-set-still-getting-default-import-error */
   globals: {
     'ts-jest': {
